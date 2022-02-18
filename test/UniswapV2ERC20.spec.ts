26476b621900f960ac357687828c46b048d17ed0
0xRison <ztshort@hotmail.com>
2022-02-18 03:06:52 -0600

    SLOTH -> MON

--- test/UniswapV2ERC20.spec.ts
+++ test/UniswapV2ERC20.spec.ts
@@ -29,8 +29,8 @@ describe('UniswapV2ERC20', () => {
 
   it('name, symbol, decimals, totalSupply, balanceOf, DOMAIN_SEPARATOR, PERMIT_TYPEHASH', async () => {
     const name = await token.name()
-    expect(name).to.eq('Sloth LP Token')
-    expect(await token.symbol()).to.eq('SLOTH-LP')
+    expect(name).to.eq('Mon LP Token')
+    expect(await token.symbol()).to.eq('MON-LP')
     expect(await token.decimals()).to.eq(18)
     expect(await token.totalSupply()).to.eq(TOTAL_SUPPLY)
     expect(await token.balanceOf(wallet.address)).to.eq(TOTAL_SUPPLY)
